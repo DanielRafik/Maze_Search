2c852d79b35bb15609bf9d130bb3f7077fe4ae07
DanielRafik <daniabdelmesih00@gmail.com>
2022-12-06 21:30:42 +0200

    sbsadbaj

--- midterm exam/midterm exam/main (1).py
+++ midterm exam/midterm exam/main (1).py
@@ -5,85 +5,56 @@ import csv
 import numpy as np
 import copy
 
+# increase the recursion limit
+import sys
+sys.setrecursionlimit(3000)
 
 
 
-def find_goal(map,length,width):
-    for i in range(length):
-        for j in range(width):
-            if map[i][j] == 2:
-                return i,j
+def find_goal(map,width):
+    the_flateen=np.asarray(map)
+    the_flateen=the_flateen.flatten()
+    for m in range(len(the_flateen)):
+        if the_flateen[m] == 2:
+            theFirstIndixVal=m//width
+            theSecondIndixVal=m-(width*theFirstIndixVal)
+            print("the First Indix Val",theFirstIndixVal,"the Second IndixVal",theSecondIndixVal)
+            return theFirstIndixVal,theSecondIndixVal
 
-
+def fill_one_point(i,j,map,length,width,theNewValue,newXandYarray):
+    if(i<length and j<width and map[i,j]==0):
+          map[i,j]=theNewValue
+          newXandYarray.append((i,j))
 def the_filling(X_goal,Y_goal,map,length,width):
-    NewXarray=([X_goal])
-    NewYarray=([Y_goal])
-    NewXarray, NewYarray =fill_the_matrix(NewXarray,NewYarray,map,length,width)
-#     print("the newNewXarray XX ",NewXarray)
-#     print("the NewYarray YY ",NewYarray)
-#     print("thelenghth of x",len(NewXarray),"thelenghth of y",len(NewYarray))
-    while(NewXarray or NewYarray ):
-        NewXarray, NewYarray =fill_the_matrix(NewXarray,NewYarray,map,length,width)
-      #   print("the newNewXarray XX ",NewXarray)
-      #   print("the NewYarray YY ",NewYarray)
-      #   print("thelenghth of x",len(NewXarray),"thelenghth of y",len(NewYarray))
-#     return map
-def fill_the_matrix(arrayX,arrayY,map,length,width):
-    NewXarray=[]
-    NewYarray=[]
-    for i in range(len(arrayX)):
-      currentXIndex=arrayX[i]
-      currentYIndex=arrayY[i]  
+    #initialize the NewXandYarray with the x index of the goal and the y index of the goal  
+    newXandYarray=[(X_goal,Y_goal)]
+    #call fill_the_matrix function with 1 array x index of the goal and the y index of the goal  
+    #it return a new array contian the indexs of the new values 
+    newXandYarray=fill_the_matrix(newXandYarray,map,length,width)
+    while(newXandYarray):
+        #recall the fill_the_matrix function untill NewXandYarray is empty
+        #when the array is empty this means no pixels fillled with the new value
+        #the matrix is all filled 
+        newXandYarray =fill_the_matrix(newXandYarray,map,length,width)
+def fill_the_matrix(oldXandYarray,map,length,width):
+    #set new array to store the x index and the y index of the values that will be filled 
+    newXandYarray=[]
+    
+    for i in range(len(oldXandYarray)):
+      currentXIndex,currentYIndex=oldXandYarray[i]
       theNewValue=map[currentXIndex,currentYIndex]+1 
-      if(currentXIndex+1<length and currentYIndex<width and map[currentXIndex+1,currentYIndex]==0):
-            map[currentXIndex+1,currentYIndex]=theNewValue
-            NewXarray.append(currentXIndex+1)
-            NewYarray.append(currentYIndex )
-            # print("there is pixeles filed with",theNewValue)
-            # print(' 1TH IF now the new YY array contian',NewYarray)
-      if(currentXIndex<length and currentYIndex+1<width and map[currentXIndex,currentYIndex+1]==0):
-            map[currentXIndex,currentYIndex+1]=theNewValue
-            NewXarray.append(currentXIndex)
-            NewYarray.append(currentYIndex+1)
-            # print("there is pixeles filed with",theNewValue)
-            # print('2TH IF now the new YY array contian',map[currentXIndex,currentYIndex+1])
-      if(currentXIndex+1<length and currentYIndex+1<width and map[currentXIndex+1,currentYIndex+1]==0):
-            map[currentXIndex+1,currentYIndex+1]=theNewValue
-            NewXarray.append(currentXIndex+1)
-            NewYarray.append(currentYIndex+1 )
-            # print("there is pixeles filed with",theNewValue)
-            # print('3TH IF now the new YY array contian',NewYarray)
-      if(currentXIndex-1<length and currentYIndex-1<width and map[currentXIndex-1,currentYIndex-1]==0):
-            map[currentXIndex-1,currentYIndex-1]=theNewValue
-            NewXarray.append(currentXIndex-1)
-            NewYarray.append(currentYIndex-1)
-            # print("there is pixeles filed with",theNewValue)
-            # print('4TH IF now the new YY array contian',NewYarray)
-      if(currentXIndex-1<length and currentYIndex<width and map[currentXIndex-1,currentYIndex]==0):
-            map[currentXIndex-1,currentYIndex]=theNewValue
-            NewXarray.append(currentXIndex-1)
-            NewYarray.append(currentYIndex)
-            # print("there is pixeles filed with",theNewValue)
-            # print('5TH IF now the new YY array contian',NewYarray)
-      if(currentXIndex<length and currentYIndex-1<width and map[currentXIndex,currentYIndex-1]==0):
-            map[currentXIndex,currentYIndex-1]=theNewValue
-            NewXarray.append(currentXIndex)
-            NewYarray.append(currentYIndex-1)
-            # print("there is pixeles filed with",theNewValue)
-            # print('6TH IF now the new YY array contian',NewYarray)
-      if(currentXIndex-1<length and currentYIndex+1<width and map[currentXIndex-1,currentYIndex+1]==0):
-            map[currentXIndex-1,currentYIndex+1]=theNewValue
-            NewXarray.append(currentXIndex-1)
-            NewYarray.append(currentYIndex+1)
-            # print("there is pixeles filed with",theNewValue)
-            # print('7TH IF now the new YY array contian',NewYarray)
-      if(currentXIndex+1<length and currentYIndex-1<width and map[currentXIndex+1,currentYIndex-1]==0):
-            map[currentXIndex+1,currentYIndex-1]=theNewValue
-            NewXarray.append(currentXIndex+1)
-            NewYarray.append(currentYIndex-1)
-            # print("there is pixeles filed with",theNewValue)
-            # print('8TH IF now the new YY array contian',NewYarray)
-    return  NewXarray,NewYarray   
+      fill_one_point(currentXIndex+1,currentYIndex,map,length,width,theNewValue,newXandYarray)
+      fill_one_point(currentXIndex,currentYIndex+1,map,length,width,theNewValue,newXandYarray)
+      
+      fill_one_point(currentXIndex+1,currentYIndex+1,map,length,width,theNewValue,newXandYarray)
+      fill_one_point(currentXIndex-1,currentYIndex-1,map,length,width,theNewValue,newXandYarray)
+      
+      fill_one_point(currentXIndex-1,currentYIndex,map,length,width,theNewValue,newXandYarray)
+      fill_one_point(currentXIndex,currentYIndex-1,map,length,width,theNewValue,newXandYarray)
+      
+      fill_one_point(currentXIndex-1,currentYIndex+1,map,length,width,theNewValue,newXandYarray)
+      fill_one_point(currentXIndex+1,currentYIndex-1,map,length,width,theNewValue,newXandYarray)
+    return  newXandYarray   
           
 def trace_back(filled_mat,x_start,y_start,x_goal,y_goal,arr_of_coordinates):
       # x,y are the starting coordinates
@@ -138,54 +109,57 @@ def trace_back(filled_mat,x_start,y_start,x_goal,y_goal,arr_of_coordinates):
       return arr_of_coordinates
 
 def planner(map,start_row,start_col):
-      if map[start_col,start_row] == 1:
-         print('forbidden place the program will start at 4,4')
-         start_col = 4
-         start_row = 4
-      length=map.shape[0]
-      width=map.shape[1]
-      x_goal,y_goal=find_goal(map,length,width)
-
-      value_map = copy.deepcopy(map)
-
-      # modifies filled_map var
-      the_filling(x_goal,y_goal,value_map,length,width)
-
-      trajectory = trace_back(value_map,start_col,start_row,x_goal,y_goal,[(x_start,y_start)])
-
-      return value_map,trajectory
+    try:
+          if start_col>=map.shape[0] or start_row>=map.shape[1]:
+             print('forbidden place the program will start at 4,4')
+             start_col = 45
+             start_row = 4
+          if map[start_col,start_row] == 1:
+             print('forbidden place the program will start at 4,4')
+             start_col = 45
+             start_row = 4
+          length=map.shape[0]
+          width=map.shape[1]
+          x_goal,y_goal=find_goal(map,width)
+    
+          value_map = copy.deepcopy(map)
+    
+          # modifies filled_map var
+          the_filling(x_goal,y_goal,value_map,length,width)
+    
+          trajectory = trace_back(value_map,start_col,start_row,x_goal,y_goal,[(start_col,start_row)])
+    
+          return value_map,trajectory
+    except:
+         print("there is an error occurred in the planner function")
+          
 
 
 
 # read input map
-mat_file = scipy.io.loadmat('maze.mat') # returns a dict
+mat_file = scipy.io.loadmat('.\maze.mat') # returns a dict
 map = mat_file['map']
 
 map=np.asarray(map)
 map=map.astype('uint32')
 
 # plot original map(maze)
-plt.imshow(map)
+plt.imshow(map,cmap='gray')
 plt.show()
 
 # get input from user
 print("###############################################################################################")
 print("'''''''FOR STARTING POINT''''''''")
+
 print("Enter a number for rows between 0 and ",map.shape[0])
 y_start = int(input())
 
 print("Enter a number for columns between 0 and ",map.shape[1])
 x_start = int(input())
-print(type(x_start))
-print(map[x_start,y_start])
-
 
 value_map , trajectory = planner(map,x_start,y_start)
-
-
-
 # plot value map
-plt.imshow(value_map,cmap='gray')
+plt.imshow(value_map,cmap='hot')
 plt.show()
 
 print(trajectory)
@@ -193,7 +167,9 @@ print(trajectory)
 
 # plot the maze with the solution path 
 for coor_tuple in trajectory:
+      #print("the cor value ",coor_tuple)
       map[coor_tuple] = 4
 plt.imshow(map,cmap='gray')
 plt.show()
+
 
